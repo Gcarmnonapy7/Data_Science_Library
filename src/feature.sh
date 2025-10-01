e7f52c201f448eddb66745dfa7a2aaeab2602401
Gabriel Carmona <191816876+Gcarmnonapy7@users.noreply.github.com>
2025-10-01 10:57:52 -0300

    first commit

---
+++ src/feature.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+BRANCH_NAME=$1
+COMMIT_MESSAGE=$2
+
+if [ -z "$BRANCH_NAME" ] || [ -z "$COMMIT_MESSAGE" ]; then
+    echo "Branch name and commit message are required."
+    exit 1
+fi
+
+echo "Creating and pushing to branch: $BRANCH_NAME with commit message: $COMMIT_MESSAGE"
+git checkout -b feature/${BRANCH_NAME} 
+    
+echo "Adding all changes in the new branch"
+git add .
+
+echo "Committing changes with message: $COMMIT_MESSAGE"
+git commit -m "$COMMIT_MESSAGE"
+    
+echo "Pushing changes to remote repository (Check the pull request)"
+git push origin ${BRANCH_NAME} 
+
+# Delete branch locally
+
+git checkout main
+
+git branch -D feature/${BRANCH_NAME}
